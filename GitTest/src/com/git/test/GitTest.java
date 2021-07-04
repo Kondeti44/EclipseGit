eca8edc3478296a3e1ca93b6aa482e51ed9efc04
Mahesh <Mahesh@MaheshBabu.attlocal.net>
2021-07-03 20:00:59 -0500

    sop removed

--- GitTest/src/com/git/test/GitTest.java
+++ GitTest/src/com/git/test/GitTest.java
@@ -5,9 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Is Fun");
-		System.out.println("Eclipse commit");
-		System.out.println("Git Commit");
-		System.out.println("Git no fetch Commit");
 	}
 
 }
